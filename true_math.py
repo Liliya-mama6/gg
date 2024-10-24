867541f6ad04101460327d05b841f7bcbe41bada
Liliya-mama6 <vozdvizhenka@mail.ru>
2024-10-24 17:07:12 +0500

    Update true_math.p

--- true_math.py
+++ true_math.py
@@ -1,27 +1,14 @@
-import sys
+import inspect
 import pprint
+
+
 def introspection_info(obj):
-    data = {}
-    data['type']= type(obj)
-    fot= list(dir(obj))
-    c=[]
-    b=[]
-    d=sys.modules[__name__]
-    s1=9
-    s2=9
-    d=str(d)
-    while d[s2]!="'":
-        s2+=1
-    e=d[s1:s2]
-    data['moduls1']=e
-    for i in fot:
-        if not i[0:2]=='__':
-            c.append(i)
-        else:
-            b.append(i)
-    data['atributs']=c
-    data['methods']=b
-    return data
+    return {
+        'type': type(obj).__name__,
+        'attributes': obj.__dir__,
+        'methods': dir(obj),
+        'module': inspect.getmodule(obj, 'sirius1.py')
+    }
 
 
 pprint.pprint(introspection_info(111))

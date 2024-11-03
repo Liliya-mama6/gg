75b2219c55090b526c3452f119688e10c4ef903e
Liliya-mama6 <vozdvizhenka@mail.ru>
2024-11-03 18:56:09 +0500

    ответочка

--- true_math.py
+++ true_math.py
@@ -1,14 +1,22 @@
-import inspect
-import pprint
+import asyncio
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-def introspection_info(obj):
-    return {
-        'type': type(obj).__name__,
-        'attributes': obj.__dir__,
-        'methods': dir(obj),
-        'module': inspect.getmodule(obj, 'sirius1.py')
-    }
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+    print('Привет! Я бот помогающий твоему здоровью.')
 
 
-pprint.pprint(introspection_info(111))
+@dp.message_handler()
+async def all_message(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+    print('Введите команду /start, чтобы начать общение.')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)

0415a5ae7c93f01761b9b2103fd96430081b103e
Liliya-mama6 <vozdvizhenka@mail.ru>
2024-11-06 21:19:43 +0500

    продвинутые экселевские таблички

--- true_math.py
+++ true_math.py
@@ -1,22 +1,35 @@
-import asyncio
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import sqlite3
 
-api = ''
-bot = Bot(token=api)
-dp = Dispatcher(bot, storage=MemoryStorage())
+contact = sqlite3.connect('dt.db')
+cursor = contact.cursor()
 
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS User(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+cursor.execute('''
+CREATE INDEX IF NOT EXISTS idx_email ON User(email)
+''')
 
-@dp.message_handler(commands=['start'])
-async def start_message(message):
-    await message.answer('Привет! Я бот помогающий твоему здоровью.')
-    print('Привет! Я бот помогающий твоему здоровью.')
+for i in range(10):
+   cursor.execute('INSERT INTO User(username, email, age, balance) VALUES (?, ?, ?, ?)',
+                 (f'user{i+1}', f'example{i+1}@gmail.com', f'{10*(i+1)}', f'{1000}'))
 
+for i in range(1, 11, 2):
+    cursor.execute('UPDATE User SET balance = ? WHERE age = ?', (500, i*10))
 
-@dp.message_handler()
-async def all_message(message):
-    await message.answer('Введите команду /start, чтобы начать общение.')
-    print('Введите команду /start, чтобы начать общение.')
+for i in range(1, 11, 3):
+    cursor.execute('DELETE FROM User WHERE age = ?', (i*10, ))
 
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+cursor.execute('SELECT username, email, age, balance FROM User WHERE age != ?', (60, ))
+users=cursor.fetchall()
+for i in users:
+    print(*i)
+
+contact.commit()
+contact.close()
